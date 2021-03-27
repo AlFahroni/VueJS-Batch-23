5c6e343a8de238a80b535d634b73f011698fc278
AlFahroni <alfahroni97@gmail.com>
2021-03-27 20:41:14 +0700

    Function Penghasil Tanggal Hari Esok

--- Quiz-1/index.js
+++ Quiz-1/index.js
@@ -14,3 +14,34 @@ jumlah_kata(kalimat_1) // 6
 jumlah_kata(kalimat_2) // 2
 
 console.log(jumlah_kata(kalimat_1) , jumlah_kata(kalimat_2))
+
+//Soal 2
+function next_date(tanggal , bulan , tahun ){
+    var newTanggal = tanggal + 1;
+    return `${newTanggal} ${bulan} ${tahun}`;
+    //maaf kak pasrah disini. butuh waktu lebih kak sepertinya, karena waktu udah mepet juga akhirnya terpaksa pakai jawaban yang ngawur ini:')
+}
+
+//contoh 1
+
+var tanggal = 29
+var bulan = 2
+var tahun = 2020
+
+console.log(next_date(tanggal , bulan , tahun )) // output : 1 Maret 2020
+
+//contoh 2
+
+var tanggal = 28
+var bulan = 2
+var tahun = 2021
+
+console.log(next_date(tanggal , bulan , tahun ))// output : 1 Maret 2021
+
+//contoh 3
+
+var tanggal = 31
+var bulan = 12
+var tahun = 2020
+
+console.log(next_date(tanggal , bulan , tahun )) // output : 1 Januari 2021
